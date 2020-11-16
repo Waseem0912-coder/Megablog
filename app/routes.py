6e8e393618982c52084bd41f31c9e98f7863a9d3
Waseem0912-coder <sherwaseeem@gmail.com>
2020-11-16 13:36:22 +0530

    Ok

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 
 @app.route('/')
@@ -21,3 +22,14 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
