125906741fc2d6010f4f757d24caeb9c2c73a394
Waseem0912-coder <sherwaseeem@gmail.com>
2020-11-20 08:26:17 +0530

    setting up flask mail encounterd error in flask shell

--- config.py
+++ config.py
@@ -12,4 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['sherwaseem@gmail.com']
-    POSTS_PER_PAGE = 10
+    POSTS_PER_PAGE = 25
