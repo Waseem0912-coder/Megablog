1a44ce2043b596835a64658050d1a94b0d2d2284
Waseem0912-coder <sherwaseeem@gmail.com>
2020-12-16 15:30:17 +0530

    going to date and time

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from config import Config
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
@@ -48,3 +49,5 @@ if not app.debug:
     app.logger.info('Microblog startup')
 
 from app import routes, models, errors
+
+bootstrap = Bootstrap(app)
