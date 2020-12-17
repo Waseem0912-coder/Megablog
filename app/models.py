b2401fec27a72e6e67c46a162f95325ead6b082f
Waseem0912-coder <sherwaseeem@gmail.com>
2020-12-17 14:18:20 +0530

    create a blueprint

--- app/models.py
+++ app/models.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 from hashlib import md5
-from app import db, login
+from time import time
+from flask import current_app
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from time import time
 import jwt
-from app import app
+from app import db, login
 
 
 followers = db.Table(
@@ -61,21 +61,23 @@ class User(UserMixin, db.Model):
                 followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
+
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+            current_app.config['SECRET_KEY'],
+            algorithm='HS256').decode('utf-8')
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY'],
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
         except:
             return
         return User.query.get(id)
 
- 
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
@@ -88,9 +90,6 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     language = db.Column(db.String(5))
 
-
     def __repr__(self):
         return '<Post {}>'.format(self.body)
-
-
-
+        return '<Post {}>'.format(self.body)
